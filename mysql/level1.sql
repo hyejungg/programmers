b8fd40e399148ddd7ef48e8d4ab7c4d92e1ec9a3
hyejung <kimhyejung12@naver.com>
2021-07-05 13:25:17 +0900

    Mod:21_7_5

--- mysql/level1.sql
+++ mysql/level1.sql
@@ -1,6 +1,6 @@
 -- # 상위 n개 레코드 출력 59405
 -- datetime을 기준으로 오름차순 정렬하여 가장 오래된 동물 1마리 출력
--- LIMIT 1을 없애면? 가장 오래된 동물부터 출력
+-- @see : LIMIT 1을 없애면? 가장 오래된 동물부터 출력
 SELECT name FROM animal_ins ORDER BY datetime ASC LIMIT 1 
 
 -- #최대값 구하기 59415
@@ -26,7 +26,7 @@ SELECT animal_id, name, datetime FROM animal_ins ORDER BY name, datetime DESC;
 -- #아픈 동물 찾기 59036
 SELECT animal_id, name FROM animal_ins WHERE intake_condition = "Sick";
 
--- #도움ㄹ의 아이디와 이름 59403
+-- #동물의 아이디와 이름 59403
 SELECT animal_id, name FROM animal_ins;
 
 -- #역순 출력하기 59035
