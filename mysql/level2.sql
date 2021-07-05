6db738f2cfd34cec6e94b863c63fc98866ed43dd
hyejung <kimhyejung12@naver.com>
2021-07-05 13:25:58 +0900

    Add:Day15_21/7/5

--- mysql/level2.sql
+++ mysql/level2.sql
@@ -4,3 +4,34 @@ SELECT datetime FROM animal_ins ORDER BY datetime ASC LIMIT 1;
 
 -- #동물 수 구하기 59406
 SELECT count(animal_id) FROM animal_ins;
+
+-- #GROUP BY
+-- #고양이와 개는 몇마리 있을까 59040
+SELECT animal_type, count(animal_type) FROM animal_ins GROUP BY animal_type ORDER BY animal_type;
+
+-- #동명 동물 찾기 59041
+SELECT name, count(name)AS COUNT FROM animal_ins 
+GROUP BY NAME HAVING NAME is not null AND COUNT >= 2 ORDER BY name;
+
+-- #입양 시각 구하기 59412
+SELECT hour(datetime) HOUR, count(datetime) COUNT FROM animal_outs 
+GROUP BY hour(datetime) HAVING HOUR >= 9 AND HOUR <= 19
+ORDER BY HOUR asc;
+
+-- #null 처리하기 59410
+-- @see : mysql은 ifnull(칼럼, 대체문자열)함수로 칼럼 값이 null인 경우 대체문자열 넣어줌
+SELECT animal_type, IFNULL(name, 'No name')name, sex_upon_intake FROM animal_ins;
+
+
+-- #datetime -> date로 변환하기 59414
+-- @see : date_foramt(칼럼, 새 포맷)함수로 포맷을 변경하여 출력 가능
+SELECT animal_id, name, date_format(datetime, '%Y-%m-%d')as '날짜' 
+FROM animal_ins ORDER BY animal_id;
+
+-- #중복저게하기 59408
+SELECT count(DISTINCT name)AS count FROM animal_ins;
+
+-- #이름에 el이 들어가는 동물 찾기 59047
+SELECT animal_id, name FROM animal_ins 
+WHERE name like '%el%' AND animal_type = 'Dog' ORDER BY name asc;
+
