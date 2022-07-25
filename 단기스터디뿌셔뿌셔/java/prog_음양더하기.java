d8f48d00bdb013ad0f5104c07d85ab66a5b8bf41
hyejungg <kimhyejung12@naver.com>
2022-07-25 19:56:01 +0900

    Add: java음양 더하기

---
+++ 단기스터디뿌셔뿌셔/java/prog_음양더하기.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        for(int i = 0; i < absolutes.length; i++){
+            if(signs[i]){
+                answer += absolutes[i];
+            }else{
+                answer -= absolutes[i];   
+            }
+        }
+        return answer;
+    }
+}
+
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        for(int i = 0; i < absolutes.length; i++){
+            answer += signs[i] ? absolutes[i] : -(absolutes[i]);
+        }
+        return answer;
+    }
+}
